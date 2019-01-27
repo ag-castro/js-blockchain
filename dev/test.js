a1ff53babc1024e82e83ef0c1e8fdcc581d14ebd
Andre Gustavo Castro <agcastro.py@yahoo.com>
2019-01-27 17:11:49 -0200

    Added the blockchain data structure.

---
+++ dev/test.js
@@ -0,0 +1,24 @@
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain();
+
+// // const previousBlockHash = 'LKDFJOIYUADF79567FDADSF875ADG';
+// // const currentBlockData = [
+// //     {
+// //         amount: 10,
+// //         sender: 'DJKFHASD8SDFS875',
+// //         recipient: 'LKDJAFA9878AD6FASD76F',
+// //     },
+// //     {
+// //         amount: 30,
+// //         sender: 'AFGSDFGSDF4GF98G4DFASD56F4',
+// //         recipient: '798SFGH4SF6GH4SFD98GSFSFD5GS',
+// //     },
+// //     {
+// //         amount: 200,
+// //         sender: '1VB9N8VCBNC98BFD5GH4SF9',
+// //         recipient: 'O9YIUOIU6O4UIO465GH4DG',
+// //     },
+// ];
+
+console.log(bitcoin);
